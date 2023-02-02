9488582722b04115e2ab4bd16746184545b58e98
Park Ji Hye <qkrwlgp235@gmail.com>
2023-02-02 18:43:46 +0900

    개인정보 수집 유효기간/ Lv1 / 추가개선2

--- ParkJiHye/JS/개인정보 수집 유효기간.js
+++ ParkJiHye/JS/개인정보 수집 유효기간.js
@@ -1,28 +1,23 @@
 function solution(today, terms, privacies) {
+	const perMonth = 28;
 	const [Tyear, Tmonth, Tday] = today.split('.').map(Number);
+
+	const todates = Tyear * 12 * 28 + Tmonth * 28 + Tday;
+
 	const termObj = terms.reduce((res, item) => {
 		const [kind, duration] = item.split(' ');
 		res[kind] = parseInt(duration);
 		return res;
 	}, {});
 
-	const result = privacies.map((p) => {
-		const [date, kind] = p.split(' ');
+	const result = privacies.reduce((acc, cur, idx) => {
+		const [date, kind] = cur.split(' ');
 		const duration = termObj[kind];
-		let [Vyear, Vmonth, Vday] = [...date.split('.').map(Number)];
+		const [year, month, day] = [...date.split('.').map(Number)];
+		const dates = year * 12 * 28 + month * 28 + day + duration * 28;
+		if (dates <= todates) acc.push(idx + 1);
+		return acc;
+	}, []);
 
-		Vmonth += duration;
-		Vyear +=
-			Vmonth % 12 === 0 ? parseInt(Vmonth / 12) - 1 : parseInt(Vmonth / 12);
-		Vmonth = Vmonth % 12 === 0 ? 12 : Vmonth % 12;
-
-		if (Vyear < Tyear) {
-			acc.push(idx + 1);
-		} else if (Vyear === Tyear && Vmonth < Tmonth) {
-			acc.push(idx + 1);
-		} else if (Vyear === Tyear && Vmonth === Tmonth && Vday <= Tday) {
-			acc.push(idx + 1);
-		}
-	});
 	return result;
 }
